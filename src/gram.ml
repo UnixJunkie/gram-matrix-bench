68c39f9c78ca12df11de18018859abdc1591aa21
Francois Berenger <unixjunkie@sdf.org>
2020-03-17 16:29:27 +0900

    WIP

---
+++ src/gram.ml
@@ -0,0 +1,26 @@
+
+(* Compute a Gram matrix as fast as possible using parallelism *)
+
+let compute_gram_matrix_seq () =
+  failwith "not implemented yet"
+
+let compute_gram_matrix_paramp () =
+  failwith "not implemented yet"
+
+let compute_gram_matrix_parany () =
+  failwith "not implemented yet"
+
+let compute_gram_matrix_multicore () =
+  failwith "waiting for external contribution"
+
+let compute_nb_features_max () =
+  failwith "not implemented yet"
+
+let read_one_line line =
+  failwith "not implemented yet"
+
+let main () =
+  (* read data in *)
+  failwith "not implemented yet"
+
+let () = main ()
